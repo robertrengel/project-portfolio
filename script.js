a81153c3e53ae619da9abb4a3e4211b2bb4baabd
robertrengel <robertrengel@gmail.com>
2023-01-11 18:31:20 -0500

    project-portfolio V0.1.2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
+const navbarButtom = document.querySelectorAll('.menu-icon')
+const navbarButtom2 = document.querySelector('.menu-options--active')
 
  
+
+navbarButtom2.addEventListener('click', (e) => {
+    e.preventDefault()
+    for (let i = 0; i <= navbarButtom.length-1; i++) {
+        if (navbarButtom[i].classList.contains('hidden')) {
+        navbarButtom[i].classList.remove('hidden');
+        console.log("Remove")
+        navbarButtom2.classList.add('hidden')
+        }
+        console.log("click")
+    }
+})
+
+console.log(navbarButtom[0])
