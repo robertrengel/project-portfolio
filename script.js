771605cb1f08e475e3f6fd506f408ced12768272
robertrengel@gmail.com <robertrengel@gmail.com>
2023-01-12 13:04:17 -0500

    project-portfolio V0.1.3

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
+const links = document.querySelectorAll('.link')
+const menuOptionsActive = document.querySelectorAll('.menu-options--active')
 
-const navbarButtom = document.querySelectorAll('.menu-icon')
-const navbarButtom2 = document.querySelector('.menu-options--active')
-
- 
-
-navbarButtom2.addEventListener('click', (e) => {
+links.forEach(link =>{
+  link.addEventListener(('click'),(e)=>{
     e.preventDefault()
-    for (let i = 0; i <= navbarButtom.length-1; i++) {
-        if (navbarButtom[i].classList.contains('hidden')) {
-        navbarButtom[i].classList.remove('hidden');
-        console.log("Remove")
-        navbarButtom2.classList.add('hidden')
-        }
-        console.log("click")
+    if(link.children[0].classList.contains('hidden')){
+      link.children[0].classList.remove('hidden')
+      link.children[1].classList.add('hidden')
+    }else if(link.children[1].classList.contains('hidden')){
+      link.children[1].classList.remove('hidden')
+      link.children[0].classList.add('hidden')
     }
-})
-
-console.log(navbarButtom[0])
+    
+    console.log(link.children[0])
+  })})
