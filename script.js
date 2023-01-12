2a390e4839d440d9411fd74646da2238a915627d
robertrengel@gmail.com <robertrengel@gmail.com>
2023-01-12 15:24:02 -0500

    project-portfolio V0.2.0

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const links = document.querySelectorAll('.link')
 const menuOptionsActive = document.querySelectorAll('.menu-options--active')
+const menuOptions = document.querySelector('.menu-options')
 
+const clearLinks = () =>{
+  links.forEach(option =>{
+   
+    option.children[0].classList.remove('hidden')
+    option.children[1].classList.add('hidden')
+  })
+}
+
+//clearLinks()
 links.forEach(link =>{
   link.addEventListener(('click'),(e)=>{
     e.preventDefault()
+    clearLinks()
     if(link.children[0].classList.contains('hidden')){
       link.children[0].classList.remove('hidden')
       link.children[1].classList.add('hidden')
@@ -11,6 +22,6 @@ links.forEach(link =>{
       link.children[1].classList.remove('hidden')
       link.children[0].classList.add('hidden')
     }
-    
-    console.log(link.children[0])
   })})
+  
+  
